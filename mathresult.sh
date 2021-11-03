6648b01c2489d101e9f25d878f7f980ed474e97b
OmarKhilji09 <93557452+OmarKhilji09@users.noreply.github.com>
2021-11-03 00:29:59 -0400

    Update mathresult.sh

--- mathresult.sh
+++ mathresult.sh
@@ -2,7 +2,11 @@
 
 
 while IFS=, read -r gen grp edu lun test math reading writing; do
-if [[ $gen != "gender" ]] && [[ $math > "64" ]]
-then echo $gen, $edu, $math
-fi
+ gen_fix=$(echo $gen | tr -d '"')
+    edu_fix=$(echo $edu | tr -d '"')
+    math_fix=$(echo $math | tr -d '"')
+
+    if [[ $gen_fix != "gender" && $math_fix > 64 ]]; then 
+        echo $gen_fix,"===" $edu_fix,"===" $math_fix
+    fi
 done < exams.csv
